bab413c3f3e09a1b6e5a87603937e82f33187e92
luizguilhermefr <luizguilhermefr@gmail.com>
2018-01-30 10:58:27 -0200

    protocol

--- app/src/main/java/br/unioeste/cropmonitor/util/Protocol.java
+++ app/src/main/java/br/unioeste/cropmonitor/util/Protocol.java
@@ -5,18 +5,24 @@ import android.support.annotation.NonNull;
 import java.math.BigDecimal;
 import java.util.Locale;
 
+import br.unioeste.cropmonitor.util.exceptions.ProtocolException;
+
 public class Protocol {
 
     public static Short MESSAGE_LEN = 8;
     public static Short SENSOR_LEN = 2;
-    public static Short VALUE_LEN = 2;
+    public static Short INTEGER_LEN = 2;
     public static Short DECIMAL_LEN = 2;
+    public static Short STATUS_LEN = 3;
+    public static Short VALUE_LEN = 5;
     public static Character READ = 'R';
     public static Character WRITE = 'W';
+    public static String OK = "OK+";
+    public static String ERROR = "ER-";
     private boolean ok = false;
     private BigDecimal value = new BigDecimal(0);
 
-    public Protocol(String message) {
+    public Protocol(String message) throws ProtocolException {
         parseMessage(message);
     }
 
@@ -25,7 +31,7 @@ public class Protocol {
         StringBuilder sensorString = new StringBuilder(MESSAGE_LEN);
         sensorString.append(READ);
         sensorString.append(String.format(Locale.ENGLISH, "%0" + SENSOR_LEN + "d", sensor));
-        sensorString.append(String.format(Locale.ENGLISH, "%" + VALUE_LEN + "." + DECIMAL_LEN + "f", (float) 0));
+        sensorString.append(String.format(Locale.ENGLISH, "%" + INTEGER_LEN + "." + DECIMAL_LEN + "f", (float) 0));
         return sensorString.toString();
     }
 
@@ -35,11 +41,34 @@ public class Protocol {
         StringBuilder sensorString = new StringBuilder(MESSAGE_LEN);
         sensorString.append(WRITE);
         sensorString.append(String.format(Locale.ENGLISH, "%0" + SENSOR_LEN + "d", sensor));
-        sensorString.append(String.format(Locale.ENGLISH, "%" + VALUE_LEN + "." + DECIMAL_LEN + "f", bg));
+        sensorString.append(String.format(Locale.ENGLISH, "%" + INTEGER_LEN + "." + DECIMAL_LEN + "f", bg));
         return sensorString.toString();
     }
 
-    private void parseMessage(String message) {
+    private void parseStatus(String status) throws ProtocolException {
+        if (status.equals(OK)) {
+            ok = true;
+        } else if (status.equals(ERROR)) {
+            ok = false;
+        } else {
+            throw new ProtocolException("Invalid status code.");
+        }
+    }
 
+    private void parseValue(String content) {
+        value = new BigDecimal(content).setScale(DECIMAL_LEN, BigDecimal.ROUND_DOWN);
+    }
+
+    private void parseMessage(String message) throws ProtocolException {
+        parseStatus(message.substring(0, STATUS_LEN - 1));
+        parseValue(message.substring(STATUS_LEN, VALUE_LEN - 1));
+    }
+
+    public boolean ok() {
+        return ok;
+    }
+
+    public BigDecimal getValue() {
+        return value;
     }
 }
