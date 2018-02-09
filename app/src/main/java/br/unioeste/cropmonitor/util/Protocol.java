ca9c683329f52ec04a5a7a3ea011d46b2b8e5a20
luizguilhermefr <luizguilhermefr@gmail.com>
2018-02-09 09:43:09 -0200

    Protocol definition

--- app/src/main/java/br/unioeste/cropmonitor/util/Protocol.java
+++ app/src/main/java/br/unioeste/cropmonitor/util/Protocol.java
@@ -1,9 +1,6 @@
 package br.unioeste.cropmonitor.util;
 
-import android.support.annotation.NonNull;
-
 import java.math.BigDecimal;
-import java.util.Locale;
 
 import br.unioeste.cropmonitor.util.exceptions.ProtocolException;
 
@@ -23,29 +20,44 @@ public class Protocol {
     private boolean ok = false;
     private BigDecimal value = new BigDecimal(0);
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Protocol 8 bytes
+    |--------------------------------------------------------------------------
+    |
+    | Incoming message: [STATUS(1)][OP_ID(1)][SENSOR(2)][INTEGER(2)][DECIMAL(2)]
+    | OP_IDS:           S (Sensor update), L (Lower threshold update), U (Upper threshold update)
+    | STATUS:           + (Success), - (Error)
+    |
+    | Example:          '+S000322' --> Sensor 00 responded with success and value 03.22
+    | Example:          '-S010000' --> Sensor 01 responded with error
+    | Example:          '+L010100' --> Lower threshold of sensor 01 updated to 01.00 with success
+    | Example:          '-U000000' --> Upper threshold of sensor 00 cannot be updated
+    |
+    | Outgoing message: [ANY(1)][OP_ID(1)][SENSOR(2)][INTEGER(2)][DECIMAL(2)]
+    | OP_IDS:           L (Lower threshold update), U (Upper threshold update)
+    |
+    | Example:          ' L010050' --> Update lower threshold of sensor 01 to 00.50
+    | Example:          ' U000350' --> Update upper threshold of sensor 00 to 03.50
+    |
+    */
+
+
     public Protocol(String message) throws ProtocolException {
         parseStatus(message.substring(0, STATUS_LEN));
         parseValue(message.substring(STATUS_LEN, VALUE_LEN).replaceAll(" ", "0"));
     }
 
-    @NonNull
-    public static String makeReadString(Integer sensor) {
-        StringBuilder sensorString = new StringBuilder(MESSAGE_LEN);
-        sensorString.append(READ);
-        sensorString.append(String.format(Locale.ENGLISH, "%0" + SENSOR_LEN + "d", sensor));
-        sensorString.append(String.format(Locale.ENGLISH, "%0" + (INTEGER_LEN + DECIMAL_LEN + 1) + "." + DECIMAL_LEN + "f", (float) 0));
-        return sensorString.toString();
-    }
-
-    @NonNull
-    public static String makeWriteString(Integer sensor, Double value) {
-        BigDecimal bg = BigDecimal.valueOf(value).setScale(DECIMAL_LEN, BigDecimal.ROUND_DOWN);
-        StringBuilder sensorString = new StringBuilder(MESSAGE_LEN);
-        sensorString.append(WRITE);
-        sensorString.append(String.format(Locale.ENGLISH, "%0" + SENSOR_LEN + "d", sensor));
-        sensorString.append(String.format(Locale.ENGLISH, "%0" + (INTEGER_LEN + DECIMAL_LEN + 1) + "." + DECIMAL_LEN + "f", bg));
-        return sensorString.toString();
-    }
+//    @NonNull
+//    public static String makeWriteString(Integer sensor, Double value) {
+//        BigDecimal bg = BigDecimal.valueOf(value).setScale(DECIMAL_LEN, BigDecimal.ROUND_DOWN);
+//        StringBuilder sensorString = new StringBuilder(MESSAGE_LEN);
+//        sensorString.append(WRITE);
+//        sensorString.append(String.format(Locale.ENGLISH, "%0" + SENSOR_LEN + "d", sensor));
+//        sensorString.append(String.format(Locale.ENGLISH, "%0" + (INTEGER_LEN + DECIMAL_LEN + 1) + "." + DECIMAL_LEN + "f", bg));
+//        return sensorString.toString();
+//    }
 
     private void parseStatus(String status) throws ProtocolException {
         if (status.equals(OK)) {
